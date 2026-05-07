d89660de771d6b2ca13358c73655eb724ce6d285
Patryk Domaszek <patryk.domaszek41@gmail.com>
2026-05-08 01:14:19 +0200

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,16 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import path from 'path'
+import { fileURLToPath } from 'url'
 
-// https://vite.dev/config/
-const defaultBase = process.env.VITE_BASE_URL || '/';
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  logLevel: 'error', // Suppress warnings, only show errors
   plugins: [react()],
-  base: defaultBase,
+  base: '/Portfolio/',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
-});
+})
